7f7dd65effd1ed761e9dcb8abd51990bb906beb5
Kadyr Atakhan <kadyr.atakhanov@gmail.com>
2023-12-04 16:37:20 +0300

    tmp

--- src/models/model.cc
+++ src/models/model.cc
@@ -13,13 +13,6 @@ void Model::InitOpenGL(RenderType type) {
 }
 
 void Model::PaintGL(RenderType type) {
-  if (type == RenderType::MAZE) {
-    render_context_->SetStrategy(new MazeRenderStrategy());
-  } else if (type == RenderType::CAVE) {
-    // context.SetStrategy(new CaveRenderStrategy());
-  } else {
-    // context.SetStrategy(new CaveRenderStrategy());
-  }
   render_context_->PaintGL();
 }
 
