8560231da239650da22cf48eb8355ed8978d6405
Kadyr Atakhan <kadyr.atakhanov@gmail.com>
2023-12-10 23:52:44 +0300

    fixed cave generator

--- src/models/cave_generator.cc
+++ src/models/cave_generator.cc
@@ -11,7 +11,7 @@ Cave CaveGenerator::Generate(size_t rows, size_t cols, float chance) {
   cave.GetMatrix() = std::vector<unsigned>(rows * cols, 0);
   cells_ = std::vector<unsigned>(cols, 0);
 
-  for (size_t row = 0; row < static_cast<size_t>(rows - 1); row++) {
+  for (size_t row = 0; row < rows; row++) {
     AddCell(cave, row, chance);
   }
   Clear();
@@ -28,7 +28,7 @@ bool CaveGenerator::RandomChoice(float chance) {
 
 void CaveGenerator::AddCell(Cave& cave, size_t row, float chance) {
   unsigned value = 0;
-  for (size_t cell = 0; cell < cave.GetCols() - 1; cell++) {
+  for (size_t cell = 0; cell < cave.GetCols(); cell++) {
     value = (RandomChoice(chance)) ? 1 : 0;
     cave.GetMatrix()[row * cave.GetCols() + cell] = value;
   }
